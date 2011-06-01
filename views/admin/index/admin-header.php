b4e00e6aa71f1f4f0658fc2e0493f0a766bf6607
David McClure <david.mcclure@virginia.edu>
2011-06-01 10:20:47 -0400

    changed css to make tabs look like real tabs, view php

--- views/admin/index/admin-header.php
+++ views/admin/index/admin-header.php
@@ -1,6 +1,6 @@
 <?php
 
-    $header = array('bodyclass' => 'bagit primary', 'title' => html_escape('BagIt | File Browser'), 'content_class' => 'horizontal-nav');
+    $header = array('bodyclass' => 'bagit primary', 'title' => html_escape('BagIt | ' . $subtitle), 'content_class' => 'horizontal-nav');
     head($header);
 
 ?>
@@ -16,12 +16,3 @@
 
 
 <h1><?php echo $header['title']; ?></h1>
-
-<ul id="section-nav" class="navigation">
-    <li class="<?php if ($liststyle == 'list') { echo 'current'; } ?>">
-        <a href="<?php echo html_escape(uri('bag-it/index/browse?view=list')); ?>">List View</a>
-    </li>
-    <li class="<?php if ($liststyle == 'hierarchy') { echo 'current'; } ?>">
-        <a href="<?php echo html_escape(uri('bag-it/index/browse?view=hierarchy')); ?>">Item Hierarchy View</a>
-    </li>
-</ul>
--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -1,3 +1,18 @@
-<?php echo $this->partial('index/admin-header.php', array('topnav' => 'create', 'liststyle' => $listStyle)); ?>
+<?php echo $this->partial('index/admin-header.php', array('topnav' => 'create', 'subtitle' => 'File Browser')); ?>
 
+<ul id="section-nav" class="navigation">
+    <li class="<?php if ($listStyle == 'list') { echo 'current'; } ?>">
+        <a href="<?php echo html_escape(uri('bag-it/index/browse?view=list')); ?>">List View</a>
+    </li>
+    <li class="<?php if ($listStyle == 'hierarchy') { echo 'current'; } ?>">
+        <a href="<?php echo html_escape(uri('bag-it/index/browse?view=hierarchy')); ?>">Item Hierarchy View</a>
+    </li>
+</ul>
 
+<div id="primary">
+
+    <?php echo flash(); ?>
+
+    <?php foot(); ?>
+
+</div>
