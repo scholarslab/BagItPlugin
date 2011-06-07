1ac70262a5f1a1e187746beb58fb9a42896f8f7f
David McClure <david.mcclure@virginia.edu>
2011-06-07 11:51:07 -0400

    added alltests.php, wrote ignition code

---
+++ tests/AllTests.php
@@ -0,0 +1,51 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Ignition for BagIt test suite.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * @package omeka
+ * @subpackage BagIt
+ * @author Scholars' Lab
+ * @author David McClure (david.mcclure@virginia.edu)
+ * @copyright 2011
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ *
+ * PHP version 5
+ *
+ */
+?>
+
+<?php
+
+require_once 'BagIt_Test_AppTestCase.php';
+
+class BagIt_AllTests extends PHPUnit_Framework_TestSuite
+{
+
+    public static function suite()
+    {
+
+        $suite = new BagIt_AllTests('BagIt Tests');
+
+        $collector = new PHPUnit_Runner_Include_PathTestCollection(
+            array(
+                dirname(__FILE__) . '/integration',
+                dirname(__FILE__) . '/unit'
+            )
+        );
+
+        $suite->addTestFiles($collector->collectTests());
+        return $suite;
+
+    }
+
+}
