d2415fb51ebd0969faf84d19cbc7d109903c5fb0
David McClure <david.mcclure@virginia.edu>
2011-06-16 08:43:10 -0400

    changed custom tables mysam -> innodb

--- plugin.php
+++ plugin.php
@@ -76,7 +76,7 @@ function bagitInstall()
             `id` int(10) unsigned NOT NULL AUTO_INCREMENT primary key,
             `name` tinytext COLLATE utf8_unicode_ci NOT NULL,
             `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        ) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
     ");
 
     $db->query("
@@ -84,7 +84,7 @@ function bagitInstall()
             `id` int(10) unsigned NOT NULL AUTO_INCREMENT primary key,
             `file_id` int(10) unsigned NOT NULL,
             `collection_id` int(10) unsigned NOT NULL
-        ) ENGINE = MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
     ");
 
 }
