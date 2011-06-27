f301fdf7c1e7af82dd6931b9e972984755dbf574
David McClure <david.mcclure@virginia.edu>
2011-06-27 14:32:21 -0400

    switched the plugin.php back to the new class-based style

--- plugin.php
+++ plugin.php
@@ -37,6 +37,7 @@ define('BAGIT_TESTS_DIRECTORY', BAGIT_PLUGIN_DIRECTORY . '/tests');
 // }}}
 
 // {{{ requires
+require_once BAGIT_PLUGIN_DIRECTORY . '/BagItPlugin.php';
 require_once BAGIT_PLUGIN_DIRECTORY . '/helpers/BagItFunctions.php';
 require_once BAGIT_PLUGIN_DIRECTORY . '/lib/BagItPHP/lib/bagit.php';
 // }}}
@@ -53,119 +54,4 @@ add_plugin_hook('admin_theme_header', 'bagitAdminThemeHeader');
 add_filter('admin_navigation_main', 'bagitAdminNavigationMain');
 // }}}
 
-/**
- * Create tables for file collections and file associations.
- *
- * @return void
- */
-function bagitInstall()
-{
-
-    $db = get_db();
-
-    $db->query("
-        CREATE TABLE IF NOT EXISTS `$db->BagitFileCollection` (
-            `id` int(10) unsigned NOT NULL AUTO_INCREMENT primary key,
-            `name` tinytext COLLATE utf8_unicode_ci NOT NULL,
-            `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            INDEX(name(60))
-        ) ENGINE = innodb DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-    ");
-
-    $db->query("
-        CREATE TABLE IF NOT EXISTS `$db->BagitFileCollectionAssociation` (
-            `id` int(10) unsigned NOT NULL AUTO_INCREMENT primary key,
-            `file_id` int(10) unsigned NOT NULL,
-            `collection_id` int(10) unsigned NOT NULL
-        ) ENGINE = innodb DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-    ");
-
-}
-
-/**
- * Drop tables.
- *
- * @return void
- */
-function bagitUninstall()
-{
-
-    $db = get_db();
-    $db->query("DROP TABLE IF EXISTS `$db->BagitFileCollection`");
-    $db->query("DROP TABLE IF EXISTS `$db->BagitFileCollectionAssociation`");
-
-}
-
-/**
- * Define access privileges.
- *
- * @param $acl The access management object passed in by the front controller.
- *
- * @return void
- */
-function bagitDefineAcl($acl)
-{
-
-    if (version_compare(OMEKA_VERSION, '2.0-dev', '<')) {
-        $indexResource = new Omeka_Acl_Resource('Bagit_Collections');
-    } else {
-        $indexResource = new Zend_Acl_Resource('Bagit_Collections');
-    }
-
-    $acl->add($indexResource);
-    $acl->allow('super', 'Bagit_Collections');
-    $acl->allow('admin', 'Bagit_Collections');
-
-}
-
-/**
- * Wire up the routes in routes.ini.
- *
- * @param object $router Router passed in by the front controller.
- *
- * @return void
- */
-function bagitDefineRoutes($router)
-{
-
-    $router->addConfig(new Zend_Config_Ini(BAGIT_PLUGIN_DIRECTORY .
-        DIRECTORY_SEPARATOR . 'routes.ini', 'routes'));
-
-}
-
-/**
- * Add custom css.
- *
- * @param object $request Page request passed in by the 'admin_theme_header'
- * hook callback.
- *
- * @return void
- */
-function bagitAdminThemeHeader($request)
-{
-
-    if ($request->getModuleName() == 'bag-it') {
-        queue_css('bagit-interface');
-    }
-
-}
-
-/**
- * Add a link to the administrative interface for the plugin.
- *
- * @param array $nav An array of main administrative links passed in
- * by the 'admin_navigation_main' filter callback.
- *
- * @return array $nav The array of links, modified to include the
- * link to the BagIt administrative interface.
- */
-function bagitAdminNavigationMain($nav)
-{
-
-    if (has_permission('Bagit_Collections', 'browse')) {
-        $nav['BagIt'] = uri('bag-it/collections');
-    }
-
-    return $nav;
-
-}
+new BagItPlugin;
