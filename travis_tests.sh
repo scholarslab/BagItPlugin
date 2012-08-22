e998f2deece71625d2e94b9445488067c739d3f2
Wayne Graham <wayne.graham@virginia.edu>
2012-08-22 16:36:24 -0400

    Update the testing framework
    Updates the testing framework to work with TravisCI.

---
+++ travis_tests.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+if [ -z $PLUGIN_DIR ]; then
+PLUGIN_DIR=`pwd`
+fi
+
+if [ -z $OMEKA_DIR ]; then
+export OMEKA_DIR=`pwd`/omeka
+  echo "omeka_dir set"
+fi
+
+cd tests/ && phpunit --configuration phpunit_travis.xml --coverage-text
