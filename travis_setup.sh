9cbde391c3e82a8e96455859fcc24dffc8d636aa
Wayne Graham <wayne.graham@virginia.edu>
2012-08-31 08:34:45 -0400

    Temporarily remove irc notification, refactor setup in to functions

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,37 +1,64 @@
 #! /usr/bin/env bash
 
-pear install pear/PEAR
-pear install pear/Archive_Tar
-phpenv rehash
-
 if [ -z $OMEKA_BRANCH ]; then
   OMEKA_BRANCH=stable-1.5
 fi
 
-export PLUGIN_DIR=`pwd`
-export OMEKA_DIR=`pwd`/omeka
+PLUGIN_DIR=`pwd`
+OMEKA_DIR=`pwd`/omeka
 
-mysql -e "create database IF NOT EXISTS omeka_test;" -uroot;
-git clone https://github.com/omeka/Omeka.git $OMEKA_DIR
+function phpDependecies {
+  pear install pear/PEAR
+  pear install pear/Archive_Tar
+  phpenv rehash
+}
 
-# check out the correct branch
-cd $OMEKA_DIR && git checkout $OMEKA_BRANCH
-cd $PLUGIN_DIR
+function createdb {
+  mysql -e "create database IF NOT EXISTS omeka_test;" -uroot;
+}
 
-# move configuration files
-mv $OMEKA_DIR/application/config/config.ini.changeme $OMEKA_DIR/application/config/config.ini
-mv $OMEKA_DIR/application/tests/config.ini.changeme $OMEKA_DIR/application/tests/config.ini
+function cloneOmeka {
+  git clone https://github.com/omeka/Omeka.git $OMEKA_DIR
+}
 
-# set up testing config
-sed -i 's/db.host = ""/db.host = "localhost"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/db.username = ""/db.username = "root"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/db.dbname = ""/db.dbname = "omeka_test"/' $OMEKA_DIR/application/tests/config.ini 
-sed -i 's/email.to = ""/email.to = "test@example.com"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/email.administator = ""/email.administrator = "admin@example.com"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/paths.maildir = ""/paths.maildir = "\/tmp"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/paths.imagemagick = ""/paths.imagemagick = "\/usr\/bin\/"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/256M/512M/' $OMEKA_DIR/application/tests/bootstrap.php
+function checkOutBranch {
+  cd $OMEKA_DIR && git checkout $OMEKA_BRANCH
+}
 
-cd $OMEKA_DIR/plugins && ln -s $PLUGIN_DIR/BagIt
-# symlink the plugin 
-cd $OMEKA_DIR/plugins && ln -s $PLUGIN_DIR
+
+function linkPlugins {
+  #cd $OMEKA_DIR/plugins && ln -s $PLUGIN_DIR/BagIt
+  cd $OMEKA_DIR/plugins && ln -s $PLUGIN_DIR
+}
+
+function moveConfigFiles {
+  mv $OMEKA_DIR/application/config/config.ini.changeme $OMEKA_DIR/application/config/config.ini
+  mv $OMEKA_DIR/application/tests/config.ini.changeme $OMEKA_DIR/application/tests/config.ini
+}
+
+
+function setUpOmeka {
+  # set up testing config
+  sed -i 's/db.host = ""/db.host = "localhost"/' $OMEKA_DIR/application/<D-d>tests/config.ini
+  sed -i 's/db.username = ""/db.username = "root"/' $OMEKA_DIR/application/tests/config.ini
+  sed -i 's/db.dbname = ""/db.dbname = "omeka_test"/' $OMEKA_DIR/application/tests/config.ini 
+  sed -i 's/email.to = ""/email.to = "test@example.com"/' $OMEKA_DIR/application/tests/config.ini
+  sed -i 's/email.administator = ""/email.administrator = "admin@example.com"/' $OMEKA_DIR/application/tests/config.ini
+  sed -i 's/paths.maildir = ""/paths.maildir = "\/tmp"/' $OMEKA_DIR/application/tests/config.ini
+  sed -i 's/paths.imagemagick = ""/paths.imagemagick = "\/usr\/bin\/"/' $OMEKA_DIR/application/tests/config.ini
+
+  sed -i 's/256M/512M/' $OMEKA_DIR/application/tests/bootstrap.php
+
+}
+
+function main {
+  phpDependecies
+  createdb
+  cloneOmeka
+  checkOutBranch
+  linkPlugins
+  moveConfigFiles
+  setUpOmeka
+}
+
+main
--- travis_tests.sh
+++ travis_tests.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/env bash
 
 if [ -z $PLUGIN_DIR ]; then
-PLUGIN_DIR=`pwd`
+  PLUGIN_DIR=`pwd`
 fi
 
 if [ -z $OMEKA_DIR ]; then
-export OMEKA_DIR=`pwd`/omeka
+  export OMEKA_DIR=`pwd`/omeka
   echo "omeka_dir set"
+  echo "Plugins `ls $OMEKA_DIR/plugins`"
 fi
 
 cd tests/ && phpunit --configuration phpunit_travis.xml --coverage-text
